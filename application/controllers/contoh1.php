3f1d906223ebcedf85160d9474e5d9a1a774f4ab
ramamahendra <ramamahendra20@gmail.com>
2023-09-26 19:37:37 +0700

    tugas pertemuan 1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Rama Mahendra
+            saya tinggal di matraman olahraga yang saya sukai adalah bola";
+    }   
+}
