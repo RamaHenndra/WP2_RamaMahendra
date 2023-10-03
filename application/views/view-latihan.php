d6b226121f92912af4b034726b765dcff5065d67
ramamahendra <ramamahendra20@gmail.com>
2023-10-03 20:31:54 +0700

    update pertemuan 3

---
+++ application/views/view-latihan.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan1</title>
+</head>
+<body>
+    Halo Kawan.. Yuks kita belajar web programming..!!<b>
+    nilai 1 = <?= $nilai1; ?>
+    nilai 2 = <?= $nilai2; ?>    
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
